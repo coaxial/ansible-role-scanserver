761f91650b1a1aed79bdc71ffd51bdc4c6f2673e
Rock <coaxial@users.noreply.github.com>
2023-06-17 15:01:00 +0200

    Install and configure saned (#2)

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,5 +6,40 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_truth(host):
-    assert True is True
+def test_sane(host):
+    assert host.package("sane").is_installed
+
+
+def test_saned_socket_config(host):
+    file = host.file("/etc/systemd/system/saned.socket")
+
+    assert file.exists
+    assert file.user == 'root'
+    assert file.group == 'root'
+    assert file.mode == 0o755
+
+
+def test_saned_service_config(host):
+    file = host.file("/etc/systemd/system/@saned.service")
+
+    assert file.exists
+    assert file.user == 'root'
+    assert file.group == 'root'
+    assert file.mode == 0o755
+
+
+def test_saned_service(host):
+    service = host.service("saned.socket")
+
+    assert service.is_enabled
+    assert service.is_running
+
+
+def test_saned_config(host):
+    file = host.file("/etc/sane.d/saned.conf")
+
+    assert file.exists
+    assert file.user == 'root'
+    assert file.group == 'root'
+    assert file.mode == 0o755
+    assert file.contains(r'^192\.168\.0\.0\/24$')
