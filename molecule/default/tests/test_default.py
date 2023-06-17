77c7a6ff45aa350e52be7ee1308d73723eee2237
coaxial <c@64b.it>
2023-06-17 14:32:37 +0200

    Configure saned

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -10,5 +10,36 @@ def test_sane(host):
     assert host.package("sane").is_installed
 
 
-def test_saned_socket_file(host):
-    assert host.file("/etc/systemd/system/saned.socket").exists
+def test_saned_socket_config(host):
+    file = host.file("/etc/systemd/system/saned.socket")
+
+    assert file.exists
+    assert file.owner == 'root'
+    assert file.group == 'root'
+    assert file.mode == 0o755
+
+
+def test_saned_service_config(host):
+    file = host.file("/etc/systemd/system/@saned.service")
+
+    assert file.exists
+    assert file.owner == 'root'
+    assert file.group == 'root'
+    assert file.mode == 0o755
+
+
+def test_saned_service(host):
+    service = host.service("saned.socket")
+
+    assert service.is_enabled
+    assert service.is_running
+
+
+def test_saned_config
+    file = host.file("/etc/sane.d/saned.conf")
+
+    assert file.exists
+    assert file.owner == 'root'
+    assert file.group == 'root'
+    assert file.mode == 0o755
+    assert '192.168.0.0/24' in file.contents
