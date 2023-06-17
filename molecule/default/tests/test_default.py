cbbfb3db03326b30017c018736bfe3baebf5c08f
coaxial <c@64b.it>
2023-06-17 14:05:59 +0200

    Install package, socket

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,5 +6,9 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_truth(host):
-    assert True is True
+def test_sane(host):
+    assert host.package("sane").is_installed
+
+
+def test_saned_socket_file(host):
+    assert host.file("/etc/systemd/system/saned.socket").exists
